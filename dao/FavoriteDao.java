3e30a9bc97e92b6166ddb39367e00cd39447e40a
nosugerhcc <51814614+nosugerhcc@users.noreply.github.com>
2020-12-31 21:13:19 +0800

    Add files via upload

---
+++ dao/FavoriteDao.java
@@ -0,0 +1,7 @@
+package cn.itcast.travel.dao;
+
+import cn.itcast.travel.domain.Favorite;
+
+public interface FavoriteDao {
+    public Favorite findByRidAndUid(int rid, int uid);
+}
---
+++ dao/impl/FavoriteDaoImpl.java
@@ -0,0 +1,26 @@
+package cn.itcast.travel.dao.impl;
+
+import cn.itcast.travel.dao.FavoriteDao;
+import cn.itcast.travel.domain.Favorite;
+import cn.itcast.travel.domain.RouteImg;
+import cn.itcast.travel.util.JDBCUtils;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class FavoriteDaoImpl implements FavoriteDao {
+    private JdbcTemplate template=new JdbcTemplate((JDBCUtils.getDataSource()));
+    @Override
+    public Favorite findByRidAndUid(int rid, int uid) {
+        Favorite favorite = null;
+        try {
+            String sql="select * from tab_favorite where rid=? and uid=? ";
+            favorite = template.queryForObject(sql, new BeanPropertyRowMapper<>(Favorite.class), rid, uid);
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+        return favorite;
+    }
+}
